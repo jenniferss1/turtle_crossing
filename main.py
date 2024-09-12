0a9ec5f971a73d0774e57f4a80326d86d0c65221
jennifer <jennifersantossouza@hotmail.com>
2024-09-12 17:35:13 -0300

    feat: Implementing the player and the car_manager

--- main.py
+++ main.py
@@ -9,9 +9,19 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    player.move()
+    
+    car_manager.create_car()
+    car_manager.move_cars()
+
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -11,8 +11,8 @@ class Player(Turtle):
         self.shape("turtle")
         self.color("green")
         self.penup()
-        self.goto(0, -270)
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
     
     def move(self):
-        new_y = self.ycor() + MOVE_DISTANCE
-        self.goto(self.xcor(), new_y)
+        self.forward(MOVE_DISTANCE)
